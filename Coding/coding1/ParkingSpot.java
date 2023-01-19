acc73358e35cc7350c455d15eb8282e5fbadaff4
LaurensBeYoung <bashine1998@outlook.com>
2023-01-19 01:18:23 -0800

    parkinglot first step

--- Coding/coding1/ParkingSpot.java
+++ Coding/coding1/ParkingSpot.java
@@ -1,1 +1,32 @@
-public class ParkingSlot
+public class ParkingSpot {
+    private Vehicle vehicle;
+    private VehicleSize spotSize;
+    private Level level;
+    private int row;
+    private int spotNumber;
+
+    public ParkingSpot(Level level, int row, int spotNumber, VehicleSize spotSize){
+        this.level = level;
+        this.row = row;
+        this.spotNumber = spotNumber;
+        this.spotSize = spotSize;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getSpotNumber() {
+        return spotNumber;
+    }
+
+    public VehicleSize getSize() {
+        return spotSize;
+    }
+
+    public isEmpty(){
+        return vehicle == null;
+    }
+
+
+}
