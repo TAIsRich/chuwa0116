68834d842c9479d7d259c39918c42d6ab2a6f85b
Sirui Wang <siri3wang@gmail.com>
2023-01-20 15:56:56 -0600

    adding simple codes for parking lot OOD problems

---
+++ Coding/coding1/Vehicle.java
@@ -0,0 +1,9 @@
+public abstract class Vehicle {
+    private String type;
+    private String car_plate;
+
+    public Vehicle(String type, String car_plate) {
+        this.type = type;
+        this.car_plate = car_plate;
+    }
+}
