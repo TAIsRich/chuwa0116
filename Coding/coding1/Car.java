86a88db1f358cf6dd553edee7d9fe064ee40c7d8
MachineLs <analysun@gmail.com>
2023-01-20 17:43:08 -0500

    add code

--- Coding/coding1/Car.java
+++ Coding/coding1/Car.java
@@ -1,2 +1,9 @@
-package Coding.coding1;public class Car {
+package Coding.coding1;
+
+public class Car {
+    public String licenseNumber;
+
+    public Car(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
 }
