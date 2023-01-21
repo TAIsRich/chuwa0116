2a3c4540f2a8278e3e3d7fc9e24d1771582692e7
Chunyu Peng <johnpeng96@ymail.com>
2023-01-20 17:30:58 -0800

    finalsub

--- Coding/Coding1/ParkingLot.java
+++ Coding/Coding1/ParkingLot.java
@@ -2,19 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class ParkingLot {
-    private Map<ParkingSlot, Vehicle> parking;
+    private Map<Integer, Vehicle> parking;
 
     public ParkingLot() {
         parking = new HashMap<>();
     }
 
     public void FillSlot(ParkingSlot slot, Vehicle vehicle) {
-        parking.put(slot, vehicle);
-        slot.SetSlot();
+        int slotNum = 0;
+        for (int s : slot.keySet()) {
+            if (!slot.get(s)) {
+                slot.SetSlot(s);
+                slotNum = s;
+                break;
+            }
+        }
+        parking.put(slotNum, vehicle);
     }
 
-    public void RemoveSlot(ParkingSlot slot) {
-        parking.remove(slot);
-        slot.RemoveSlot();
+    public void RemoveSlot(ParkingSlot slot, int slotNum) {
+        parking.remove(slotNum);
+        slot.RemoveSlot(slotNum);
     }
 }
