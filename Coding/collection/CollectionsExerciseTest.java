e241e1b942c7b2da4e057283679ecf84812c7775
LaurensBeYoung <bashine1998@outlook.com>
2023-01-25 12:35:01 -0800

    Update CollectionsExerciseTest.java

--- Coding/collection/CollectionsExerciseTest.java
+++ Coding/collection/CollectionsExerciseTest.java
@@ -1,5 +1,7 @@
 package com.chuwa.exercise.collection;
 
+import java.util.Collections;
+
 import org.junit.Test;
 
 /**
@@ -20,6 +22,16 @@ public class CollectionsExerciseTest {
 
     @Test
     public void learn_common_collections_operations() {
+        int[] values = new int[]{ 8, 2, 3, 7, 5, 6, 7, 5, 9, 9, 16 };
+        List<Integer> l = Arrays.asList(values);
+
+        int min = Collections.min(l);
+        int max = Collections.max(l);
+        int freq = Collections.frequency(l, 9);
+        
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(freq);
 
     }
 
@@ -29,6 +41,14 @@ public class CollectionsExerciseTest {
 
     @Test
     public void learn_thread_safe_ArrayList() {
+        try {
+            int[] values = new int[]{ 8, 2, 3, 7, 5, 6, 7, 5, 9, 9, 16 };
+            List<Integer> l = Arrays.asList(values);
 
+            List<Integer> l2 = Collections.synchronizedList(l);
+        }  catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+            
     }
 }
