87a994a7a39f33685ed206782f7ea25124546c40
LaurensBeYoung <bashine1998@outlook.com>
2023-01-25 14:00:22 -0800

    Update HashSetExerciseTest.java

--- Coding/collection/HashSetExerciseTest.java
+++ Coding/collection/HashSetExerciseTest.java
@@ -27,6 +27,19 @@ public class HashSetExerciseTest {
 
     @Test
     public void learn_Inserting_And_Retrieving_Removing() {
+        Set<String> colors = new HashSet<>();
+        colors.add("red");
+        colors.add("blue");
+        colors.add("pink");
+        System.out.println(colors.size());
 
+        colors.remove("red");
+        System.out.println(colors.size());
+
+        System.out.println(colors.contains("red"));
+        System.out.println(colors.isEmpty());
+
+        colors.clear();
+        System.out.println(colors.isEmpty());
     }
 }
