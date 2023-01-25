2e743af7f27bcb3dfdac6cbb4c92821415234299
LaurensBeYoung <bashine1998@outlook.com>
2023-01-25 14:48:18 -0800

    Update TreeSetExerciseTest.java

--- Coding/collection/TreeSetExerciseTest.java
+++ Coding/collection/TreeSetExerciseTest.java
@@ -1,5 +1,8 @@
 package com.chuwa.exercise.collection;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.junit.Test;
 
 /**
@@ -32,6 +35,21 @@ public class TreeSetExerciseTest {
 
     @Test
     public void learn_Inserting_And_Retrieving_Removing() {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(1);
+        ts.add(9);
+        ts.add(15);
+        ts.add(28);
+        ts.add(90);
+
+        ts.subSet(9, 28);
+        System.out.println(ts.first());
+        System.out.println(ts.last());
+        System.out.println(ts.size());
+
+        SortedSet<Integer> h = ts.headSet(28);
+        System.out.println(h.size());
+
 
     }
 }
