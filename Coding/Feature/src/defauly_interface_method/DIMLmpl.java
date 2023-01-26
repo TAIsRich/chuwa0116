1b170d29826ff6e23683e7085ad7a1dfecac1d78
yanrong huang <huangyanr98@gmail.com>
2023-01-26 18:38:36 -0500

    features

---
+++ Coding/Feature/src/defauly_interface_method/DIMLmpl.java
@@ -0,0 +1,8 @@
+package defauly_interface_method;
+
+public class DIMLmpl implements DIML{
+    @Override
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ Coding/Feature/src/stream_api/StreamAPICreation.java
@@ -0,0 +1,85 @@
+package stream_api;
+
+import optional.Employee;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class StreamAPICreation {
+    @Test
+    public void testCollection() {
+        //col.stream
+        List<Integer> col = Arrays.asList(1, 2, 3, 4, 5);
+        col.forEach(System.out::println);
+
+        // 顺序流
+        Stream<Integer> stream = col.stream();
+        System.out.println("Stream: " + stream);
+
+        // 并行流
+        Stream<Integer> parallelStream = col.parallelStream();
+        System.out.println("Parallel Stream: " + parallelStream);
+    }
+
+    @Test
+    public void testArrays() {
+        //Array stream
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        IntStream stream = Arrays.stream(arr);
+        System.out.println("Stream object:      " + stream);
+
+        Employee e1 = new Employee(1001, "tom", 20, 2222);
+        Employee e2 = new Employee(1002, "Jerry", 18, 5000);
+        Employee[] arr1 = new Employee[]{e1, e2};
+        Stream<Employee> stream1 = Arrays.stream(arr1);
+        System.out.println("Stream object:     " + stream1);
+    }
+
+    @Test
+    public void testOf() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+        System.out.println("Stream object:      " + stream);
+    }
+
+    @Test
+    public void testIterate() {
+        //遍历前十个偶数
+        Stream.iterate(0, t -> t + 2).limit(10).forEach(System.out::println);
+    }
+
+    @Test
+    public void testGenerate() {
+        //取随机数
+        Stream.generate(Math::random).limit(10).forEach(System.out::println);
+
+        Stream<String> streamGenerated =
+                Stream.generate(() -> "element").limit(10);
+        streamGenerated.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testBuilder() {
+        //用Builder构造
+        Stream<String> streamBuilder =
+                Stream.<String>builder().add("a").add("b").add("c").build();
+        System.out.println("Stream Object:  " + streamBuilder);
+        streamBuilder.forEach(System.out::println);
+    }
+
+    @Test
+    public void testStreamOfPrimitives() {
+        IntStream intStream = IntStream.range(1, 3);
+        LongStream longStream = LongStream.rangeClosed(1, 3);
+
+        System.out.println("Stream Object:  " + intStream);
+        intStream.forEach(System.out::println);
+
+        System.out.println("Stream Object:  " + longStream);
+        longStream.forEach(System.out::println);
+    }
+}
