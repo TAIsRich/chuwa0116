0810f9b024f8fd4052a57d03988a9bb855df7594
yuchengyang0918 <yangyucheng0918@gmail.com>
2023-01-26 11:45:31 -0500

    HW5 first commit

---
+++ chuwa/src/main/java/org/example/java8Features/lambda/LambdaLearn.java
@@ -0,0 +1,81 @@
+package org.example.java8Features.lambda;
+
+import org.junit.Test;
+
+public class LambdaLearn {
+
+    @Test
+    public void overrideFoo() {
+        Foo fooByIC = new Foo() {
+            @Override
+            public String aMethod(String string) {
+                return string + " from Foo";
+            }
+        };
+
+        String hello = fooByIC.aMethod("hello");
+        System.out.println(hello);
+    }
+
+    @Test
+    public void lambdaFoo() {
+        Foo foo = parameter -> parameter + " from Foo";
+
+        String hello = foo.aMethod("hello");
+        System.out.println(hello);
+    }
+
+    @Test
+    public void lambdaFoo2() {
+        Foo foo = parameter -> parameter.toUpperCase() + " from Foo";
+
+        String hello = foo.aMethod("hello");
+        System.out.println(hello);
+    }
+
+    @Test
+    public void testFinal() {
+        final String localVariable = "Local";
+        Foo foo = parameter -> {
+            return parameter + " " + localVariable;
+        };
+
+        System.out.println(foo.aMethod("hello"));
+    }
+
+    @Test
+    public void testEffectivelyFinal() {
+        String localVariable = "Local";
+        Foo foo = parameter -> {
+            return parameter + " " + localVariable;
+        };
+
+        System.out.println(foo.aMethod("hello"));
+    }
+
+    @Test
+    public void testFinal21() {
+        String localVariable = "Local";
+        localVariable = "LOCAL";
+
+        Foo foo = parameter -> {
+            return parameter;
+        };
+
+        System.out.println(foo.aMethod("hello"));
+    }
+
+    @Test
+    public void testFinal22() {
+        String localVariable = "Local";
+
+        Foo foo = parameter -> {
+//            return parameter + " " + localVariable;
+            return parameter;
+        };
+
+        localVariable = "LOCAL";
+
+        System.out.println(foo.aMethod("hello"));
+    }
+}
