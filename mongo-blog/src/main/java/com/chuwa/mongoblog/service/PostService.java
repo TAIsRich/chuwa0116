6751b103080a0b66ce7bf1dd6d7de7e364d54132
Zhipeng yin <zhip_y@yeah.net>
2023-02-03 21:07:40 -0500

    Zhipeng/hw8

---
+++ mongo-blog/src/main/java/com/chuwa/mongoblog/service/PostService.java
@@ -0,0 +1,13 @@
+package com.chuwa.mongoblog.service;
+
+
+import com.chuwa.mongoblog.payload.PostDTO;
+
+import java.util.List;
+
+public interface PostService {
+    PostDTO createPost(PostDTO postDTO) ;
+
+
+
+}
