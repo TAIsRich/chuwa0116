4bb909f2b771c40fb80fb20720b09bb4e92a27b0
Congshan <79315517+sapphire1122@users.noreply.github.com>
2023-02-03 20:40:52 -0500

    Finish the hw of DATABASE, API and Spring.

---
+++ Coding/redbook/src/main/java/com/chuwa/redbook/service/Impl/PostServiceImpl.java
@@ -0,0 +1,85 @@
+package com.chuwa.redbook.service.Impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.exception.ResourceNotFoundException;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;//不需要在extend repository了
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //covert postDTO to Post
+        //如果entity比较复制，会另外写方法来解决
+        Post post = mapToEntity(postDto);
+
+        Post savedPost = postRepository.save(post);
+
+        //convert Post back to postDTO
+        PostDto postResponse = mapToDTO(savedPost);
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPost(){
+        List<Post> posts = postRepository.findAll();
+        List<PostDto> postDtos = posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+        return postDtos;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDTO(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return mapToDTO(updatePost);
+    }
+
+
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        postDto.setId(postDto.getId());
+        postDto.setTitle(postDto.getTitle());
+        postDto.setDescription(postDto.getDescription());
+        postDto.setContent(postDto.getContent());
+
+        return post;
+    }
+}
