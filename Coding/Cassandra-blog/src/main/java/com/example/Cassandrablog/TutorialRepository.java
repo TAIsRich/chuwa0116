d1fec10a65967ec880636b15db5f32ec575deff6
Hongri Yan <41353551+MasonYDL@users.noreply.github.com>
2023-02-05 22:54:29 -0500

    update

---
+++ Coding/Cassandra-blog/src/main/java/com/example/Cassandrablog/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.example.Cassandrablog;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+public interface TutorialRepository extends CassandraRepository<Tutorial, UUID> {
+    @AllowFiltering
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
