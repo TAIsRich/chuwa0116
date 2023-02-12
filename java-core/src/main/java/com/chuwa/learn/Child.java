09f2bd66d91a3a6ef35fc2832cf91aa46d16e378
twu <tbnghjan@gmail.com>
2023-02-12 15:43:12 -0600

    hw10

---
+++ java-core/src/main/java/com/chuwa/learn/Child.java
@@ -0,0 +1,9 @@
+package com.chuwa.learn;
+
+public class Child extends Parent {
+    String y = "child";
+
+    public void print() {
+        System.out.println(y);
+    }
+}
---
+++ java-core/src/main/java/com/chuwa/learn/Parent.java
@@ -0,0 +1,13 @@
+package com.chuwa.learn;
+
+public class Parent {
+    String x = "parent";
+
+    public void print() {
+        System.out.println(x);
+    }
+
+    public void printx() {
+        System.out.println("hello");
+    }
+}
