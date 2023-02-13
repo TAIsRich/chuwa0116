fecdf4ab252132fcba991d02efbc37fc0142dfe5
Hao Jia <haojia311@gmail.com>
2023-02-13 17:11:05 -0600

    hw42-spring-data-0213

---
+++ Coding/redbook/tree/01_post_C/src/main/java/com/chuwa/redbook/service/Impl/PostServiceImpl.java
@@ -0,0 +1,51 @@
+package com.chuwa.redbook.service.Impl;
+
+import com.chuwa.redbook.dao.PostRepository;
+import com.chuwa.redbook.entity.Post;
+import com.chuwa.redbook.payload.PostDto;
+import com.chuwa.redbook.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // convert postDTO to post
+        Post post = mapToEntity(postDto);
+
+        // save Post to db
+        Post savedPost = postRepository.save(post);
+
+        // convert Post back to postDTO
+        PostDto postResponse = mapToDTO(savedPost);
+
+        return postResponse;
+    }
+
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        return post;
+    }
+}
